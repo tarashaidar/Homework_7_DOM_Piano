24fd6d77a9a575a470da5dc067bd3363ad8a5091
Taras <47187659+tarashaidar@users.noreply.github.com>
2019-04-06 14:59:42 +0300

    Update piano.js

--- js/piano.js
+++ js/piano.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll("button");
 const audio = document.querySelectorAll(".myAudio");
 const BACKGROUND = "linear-gradient(rgb(239, 239, 239), rgb(204, 204, 204))";
 
+
+
 function playAudio(e) {
     switch(e.keyCode){
         case 65:
@@ -39,13 +41,63 @@ function playAudio(e) {
     }
 
 
-    function defaultBackround() {
+
+
+    function setDefaultBackround() {
       buttons.forEach((button)=>{
           button.style.background = "white"; 
       });
   }
-  setTimeout(defaultBackround,100);
+  setTimeout(setDefaultBackround,100);
      
 }
 
+
+
+
 addEventListener(`keydown`,  playAudio );
+
+buttons[0].addEventListener(`mousedown`, () => {
+    audio[0].play();
+    buttons[0].style.background = BACKGROUND;
+});
+buttons[1].addEventListener(`mousedown`, () => {
+    audio[1].play();
+    buttons[1].style.background = BACKGROUND;
+});
+buttons[2].addEventListener(`mousedown`, () => {
+    audio[2].play();
+    buttons[2].style.background = BACKGROUND;
+});
+buttons[3].addEventListener(`mousedown`, () => {
+    audio[3].play();
+    buttons[3].style.background = BACKGROUND;
+});
+buttons[4].addEventListener(`mousedown`, () => {
+    audio[4].play();
+    buttons[4].style.background = BACKGROUND;
+});
+buttons[5].addEventListener(`mousedown`, () => {
+    audio[5].play();
+    buttons[5].style.background = BACKGROUND;
+});
+buttons[6].addEventListener(`mousedown`, () => {
+    audio[6].play();
+    buttons[6].style.background = BACKGROUND;
+});
+buttons[7].addEventListener(`mousedown`, () => {
+    audio[7].play();
+    buttons[7].style.background = BACKGROUND;
+});
+
+addEventListener(`mouseup`, () =>{
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.background = "white"; 
+    }
+} );
+
+
+
+
+
+
